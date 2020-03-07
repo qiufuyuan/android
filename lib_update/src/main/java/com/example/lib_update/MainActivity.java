0ec286e6bb802947a94079bce278832b5f7f3b4e
qiufuyuan <526230307@qq.com>
2020-03-07 15:50:57 +0800

    创建工程

---
+++ lib_update/src/main/java/com/example/lib_update/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lib_update;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
